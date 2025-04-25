9e1398b7a7c5ef3d4ce72dc6a95af3983e6d713e
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-04-25 13:21:00 +0200

    Fixes

--- src/main/java/com/artillexstudios/axvouchers/config/Config.java
+++ src/main/java/com/artillexstudios/axvouchers/config/Config.java
@@ -1,6 +1,7 @@
 package com.artillexstudios.axvouchers.config;
 
 import com.artillexstudios.axapi.config.YamlConfiguration;
+import com.artillexstudios.axapi.config.annotation.Comment;
 import com.artillexstudios.axapi.config.annotation.ConfigurationPart;
 import com.artillexstudios.axapi.database.DatabaseConfig;
 import com.artillexstudios.axapi.libs.snakeyaml.DumperOptions;
@@ -12,11 +13,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Config implements ConfigurationPart {
+    @Comment("""
+            WARNING! This only works for UNSTACKABLE vouchers!
+            With this setting, we give each voucher an UUID,
+            and prohibit the reuse of vouchers
+            
+            PLEASE READ! This setting does NOT support copying items in creative!
+            If you want to do that, don't. Use the command to give the player a new voucher.
+            """)
     public static boolean dupeProtection = true;
+    @Comment("Whether to prevent the use of vouchers in crafting recipes")
     public static boolean preventCrafts = true;
+    @Comment("If we should send a message when a requirement fails")
     public static boolean sendRequirementFail = true;
     public static DatabaseConfig database = new DatabaseConfig();
+    @Comment("""
+            If we should send debug messages in the console
+            You shouldn't enable this, unless you want to see what happens in the code
+            """)
     public static boolean debug = false;
+    public static int configVersion = 1;
     private static final Config INSTANCE = new Config();
     private YamlConfiguration config = null;
 
